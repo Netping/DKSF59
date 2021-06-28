68024080076c27133f0be5a0627a80f585a78307
ivanov <ivanov@csort.ru>
2021-06-29 01:41:05 +0700

    Релиз после тестирования и справления утечек памяти

--- final app/pj4/Core/Core/Inc/ntp.h
+++
@@ -1,8 +0,0 @@
-#include "cmsis_os.h"
-#ifndef NTP_H_
-#define NTP_H_
-//--------------------------------------------------
-//--------------------------------------------------
-#endif /* NTP_H_ */
-void ntp_thread(void const * argument);
-extern osThreadId ntp_task_id;
